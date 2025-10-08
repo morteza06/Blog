b17884a3c40166595ef9fdd1c4c2f7285e9c631a
morteza06 <mgahangir2006@gmail.com>
2025-10-08 17:53:14 +0330

    feat: add signup and login to account, add model to blog

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()         # از UserCreationForm استفاده می‌کند
+            login(request, user)       # بعد از ثبت‌نام، کاربر وارد می‌شود
+            return redirect("/")       # یا هر صفحه دیگری
+    else:
+        form = SignUpForm()
+    return render(request, "accounts/signup.html", {"form": form})
