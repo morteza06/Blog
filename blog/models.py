9f58e502d04e4c473202628e90c80162fae92f1f
morteza06 <mgahangir2006@gmail.com>
2025-10-11 10:38:25 +0330

    chore: refactor blog model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,20 @@
-from django.conf import settings
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.urls import reverse
+
+
+class Tag(models.Model):
+    name = models.CharField(unique=True, max_length=50)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -10,9 +25,34 @@ class Post(models.Model):
         related_name='posts'
     )
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.post}"
